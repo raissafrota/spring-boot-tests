7917dbbe506a1b19806f3d37532248fc35de58ef
Raissa Frota <raissa_queiroz@hotmail.com>
2023-01-16 20:33:44 -0300

    Criando a classe User

---
+++ src/main/java/br/com/raissafrota/springboottests/domain/User.java
@@ -0,0 +1,28 @@
+package br.com.raissafrota.springboottests.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Integer id;
+
+    private String nome;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+}
