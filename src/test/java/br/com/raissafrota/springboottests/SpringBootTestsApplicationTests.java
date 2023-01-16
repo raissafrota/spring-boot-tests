7e68ef90eb1dbc98c6288fe27f351be47cd93d05
Raissa Frota <raissa_queiroz@hotmail.com>
2023-01-16 20:26:07 -0300

    Criando o projeto em Spring Boot

---
+++ src/test/java/br/com/raissafrota/springboottests/SpringBootTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.raissafrota.springboottests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
