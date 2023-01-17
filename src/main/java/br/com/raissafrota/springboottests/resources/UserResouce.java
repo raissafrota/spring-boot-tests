f0338388f18be3963bcee28e27e9dd9f819c8645
Raissa Frota <raissa_queiroz@hotmail.com>
2023-01-17 10:42:23 -0300

    Ajustando método findById para retornar um UserDto

--- src/main/java/br/com/raissafrota/springboottests/resources/UserResouce.java
+++ src/main/java/br/com/raissafrota/springboottests/resources/UserResouce.java
@@ -1,7 +1,9 @@
 package br.com.raissafrota.springboottests.resources;
 
 import br.com.raissafrota.springboottests.domain.User;
+import br.com.raissafrota.springboottests.dto.UserDTO;
 import br.com.raissafrota.springboottests.services.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,9 +18,12 @@ public class UserResouce {
     @Autowired
     UserService service;
 
+    @Autowired
+    ModelMapper modelMapper;
+
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable Integer id){
-        return ResponseEntity.ok().body(service.findById(id));
+    public ResponseEntity<UserDTO> findById(@PathVariable Integer id){
+        return ResponseEntity.ok().body(modelMapper.map(service.findById(id), UserDTO.class));
     }
 
 }
