ce6ac70efa3c120c96fb7fc2051c9d9fe01b4f85
Raíssa Frota <raissa_queiroz@hotmail.com>
2023-01-20 11:18:26 -0300

    Testes para a classe User Resouce método findAll

--- src/test/java/br/com/raissafrota/springboottests/resources/UserResouceTest.java
+++ src/test/java/br/com/raissafrota/springboottests/resources/UserResouceTest.java
@@ -8,6 +8,8 @@ import static org.mockito.Mockito.when;
 import br.com.raissafrota.springboottests.domain.User;
 import br.com.raissafrota.springboottests.dto.UserDTO;
 import br.com.raissafrota.springboottests.services.Impl.UserServiceImpl;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,6 +18,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 @SpringBootTest
@@ -64,7 +67,23 @@ class UserResouceTest {
     }
 
     @Test
-    void findAll() {
+    void whenFindAllThenReturnAListOfUserDTO() {
+        when(service.findAll()).thenReturn(List.of(user));
+        when(mapper.map(any(), any())).thenReturn(userDTO);
+
+        ResponseEntity<List<UserDTO>> response = resource.findAll();
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(ArrayList.class, response.getBody().getClass());
+        assertEquals(UserDTO.class, response.getBody().get(INDEX).getClass());
+
+        assertEquals(ID, response.getBody().get(INDEX).getId());
+        assertEquals(NAME, response.getBody().get(INDEX).getName());
+        assertEquals(EMAIL, response.getBody().get(INDEX).getEmail());
+        assertEquals(PASSWORD, response.getBody().get(INDEX).getPassword());
     }
 
     @Test
